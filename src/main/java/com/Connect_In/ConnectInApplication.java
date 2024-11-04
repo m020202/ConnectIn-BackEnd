89016da14c79cbbdea756a79762414ce1c88990e
m020202 <m020202@naver.com>
2024-11-04 23:27:58 +0900

    feat: initial commit

---
+++ src/main/java/com/Connect_In/ConnectInApplication.java
@@ -0,0 +1,13 @@
+package com.Connect_In;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectInApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectInApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Connect_In/ConnectInApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Connect_In;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectInApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
